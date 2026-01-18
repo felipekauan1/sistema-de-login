f087da9eb3e924fabd6d594e43c29061198d40a2
felipekauan1 <felipekauan30@outlook.com>
2026-01-18 17:12:02 -0300

    feat: adiciona controle de sessão no sistema de login

--- sistema.php
+++ sistema.php
@@ -1,3 +1,24 @@
 <?php
-    echo "Bem-vindo!";
-?>
+    session_start();
+
+    print_r($_SESSION);
+
+    if ((!isset($_SESSION["email"]) == true) and (!isset($_SESSION["email"]) == true)) {
+        unset($_SESSION["email"]);
+        unset($_SESSION["senha"]);
+        
+        header('Location: login.php');
+    }
+    
+    $logado = $_SESSION["email"];
+?><!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema | FK</title>
+</head>
+<body>
+    <h1>Acessou o sistema</h1>
+</body>
+</html>
--- teste-login.php
+++ teste-login.php
@@ -1,10 +1,12 @@
 <?php
+    session_start();
+
     if (isset($_POST["submit"]) && !empty($_POST["email"]) && !empty($_POST["senha"])) {
         // Acessa
         include_once('config.php');
 
-        $email = $_POST['email'];
-        $senha = $_POST['senha'];
+        $email = $_POST["email"];
+        $senha = $_POST["senha"];
 
         /*print_r($email);
         echo "<br>";
@@ -15,8 +17,14 @@
         $result = $conexao->query($sql);
 
         if (mysqli_num_rows($result) < 1) {
+            unset($_SESSION["email"]);
+            unset($_SESSION["senha"]);
+
             header('Location: login.php');
         } else {
+            $_SESSION["email"] = $email;
+            $_SESSION["senha"] = $senha;
+
             header('Location: sistema.php');
         }
     } else {
