11d322bbcc4201e35a1c2c754bd9189200450a64
felipekauan1 <felipekauan30@outlook.com>
2026-01-18 17:40:21 -0300

    fix: adiciona campo de senha no cadastro de usuários

--- cadastro.php
+++ cadastro.php
@@ -4,6 +4,7 @@
 
         $nome = $_POST["nome"];
         $email = $_POST["email"];
+        $senha = $_POST["senha"];
         $telefone = $_POST["telefone"];
         $sexo = $_POST["sexo"];
         $data_nasc = $_POST["data_nasc"];
@@ -11,7 +12,7 @@
         $estado = $_POST["estado"];
         $endereco = $_POST["endereco"];
 
-        $result = mysqli_query($conexao, "INSERT INTO usuarios (nome, email, telefone, sexo, data_nasc, cidade, estado, endereco) VALUES ('$nome', '$email', '$telefone', '$sexo', '$data_nasc', '$cidade', '$estado', '$endereco')");
+        $result = mysqli_query($conexao, "INSERT INTO usuarios (nome, email, senha, telefone, sexo, data_nasc, cidade, estado, endereco) VALUES ('$nome', '$email', '$senha', '$telefone', '$sexo', '$data_nasc', '$cidade', '$estado', '$endereco')");
     }
 ?><!DOCTYPE html>
 <html lang="pt-BR">
