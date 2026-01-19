aa889c2be818795ab3d93c398d8d378e49a58b46
felipekauan1 <felipekauan30@outlook.com>
2026-01-19 16:43:45 -0300

    style: adiciona favicon às telas do sistema

--- cadastro.php
+++ cadastro.php
@@ -23,6 +23,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulário - FK</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 </head>
 <body>
     <div class="container">
--- sistema.php
+++ sistema.php
@@ -23,6 +23,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema | FK</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <style>
         * {
             margin: 0;
